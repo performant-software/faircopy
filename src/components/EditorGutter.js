095bb8c525d879a962bb599a557e7cb42533a93f
Nick Laiacona <nick@performantsoftware.com>
2019-07-25 09:28:07 -0400

    make editor gutter aware

--- src/components/EditorGutter.js
+++ src/components/EditorGutter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 const gutterTop = 60
+const validStructureTags = ['paragraph','lineGroup']
 
 export default class EditorGutter extends Component {
 
@@ -12,15 +13,17 @@ export default class EditorGutter extends Component {
 
         const gutterMarks = []
         editorState.doc.descendants( (node,pos) => {
-            if( node.type.name === 'paragraph' ) {
+            const structureTag = node.type.name
+            if( validStructureTags.includes( structureTag ) ) {
                 const startCoords = editorView.coordsAtPos(pos)
                 const endCoords = editorView.coordsAtPos(pos + node.nodeSize)
                 const top = startCoords.top - gutterTop + scrollTop
                 const height = endCoords.bottom - startCoords.top - 8 
                 const markStyle = { top, height }
                 const markKey = `gutter-mark-${gutterMarks.length}`
+                const className = `marker ${structureTag}`
                 gutterMarks.push(
-                    <div key={markKey} style={markStyle} className='paragraph-marker'></div>
+                    <div key={markKey} style={markStyle} className={className}></div>
                 )                        
                 return false
             }
