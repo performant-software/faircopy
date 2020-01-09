549410eb7e2ea8bc1c02b86db587777401dc3877
Nick Laiacona <nick@performantsoftware.com>
2020-01-08 19:29:33 -0500

    Implement GraphWindow to graph TEI Simple structure
    * demo force dir graph
    
    * wip - analyzing tei
    
    * graph element classes
    
    * graphing members wip
    
    * load deps
    
    * take refs into account
    
    * latest

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 import MainWindow from './MainWindow'
 import NoteWindow from './NoteWindow'
+import GraphWindow from './GraphWindow'
 
 export default class App extends Component {
   
   render() {
-    if( window.fairCopy.rootComponent === "MainWindow" ) {
+    const {rootComponent} = window.fairCopy
+
+    if( rootComponent === "MainWindow" ) {
         return (
             <MainWindow></MainWindow>
         )
-    } else {
+    } else if( rootComponent === "NoteWindow" ) {
         return (
             <NoteWindow></NoteWindow>
         )
+    } else {
+      return (
+        <GraphWindow></GraphWindow>
+      )
     }
 
   }
