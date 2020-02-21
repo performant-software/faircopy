6c4329d58710e2ad96a7aa3c4ac15dd314f84697
Nick Laiacona <nick@performantsoftware.com>
2020-02-21 07:54:25 -0500

    allow attributes

--- scripts/turtle.js
+++ scripts/turtle.js
@@ -87,10 +87,7 @@ function loadModule( moduleName ) {
         for (let i = 0; i < tags.length; i++) {
             const tagEl = tags[i]
             if( keyTag === tagEl.localName )  {
-                const key = tagEl.getAttribute('key')
-                if( !key.startsWith('att.') ) {
-                    keys.push( key )    
-                }
+                keys.push( tagEl.getAttribute('key') )    
             }
         } 
         return keys   
