4dd1d2a4969655f61bf008f9ab2d7ba81e6e07ce
Nick Laiacona <nick@performantsoftware.com>
2020-03-09 11:41:33 -0400

    Load attributes from ODD, upgrade to Electron 8.1.0
    * load ODD specs and their deps
    
    * first draft of loading attrs
    
    * generate config with attributes
    
    * improve fonts
    
    * temporary fix for pbs and notes
    
    * recurse class specs

---
+++ scripts/turtle/index.js
@@ -0,0 +1,244 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+// const csv = require('csvtojson');
+
+// this is https://github.com/TEIC/TEI
+const teiSpecsDir = '../TEI/P5/Source/Specs'
+
+const {parseSpecs} = require('./parse-specs');
+
+// const teiSimplePrintODD = 'scripts/tei_simplePrint.odd'
+
+const phraseMarks = [
+    "ref",
+    "name",
+    "rs",
+    "num",
+    "measure",
+    "time",
+    "date",
+    "email",
+    "expan",
+    "abbr",
+    "orig",
+    "supplied",
+    "corr",
+    "reg",
+    "unclear",
+    "del",
+    "sic",
+    "add",
+    "term",
+    "foreign",
+    "title",
+    "hi",
+    "rhyme",
+    "seg",
+    "s",
+    "measure"
+]
+
+const examplarEls = [
+    "div",
+    "p",
+    "pb",
+    "note"
+]
+
+const dramaEls = [
+    // "actor",
+    // "castGroup",
+    // "castItem",
+    // "castList",
+    // "set",
+    "sp",
+    // "speaker",
+    // "stage",
+    // "role",
+    // "roleDesc"
+]
+
+
+// load simple file, locate body els, make a list of their modules
+// function loadTEISimpleElements() {
+//     const teiSimpleXML = fs.readFileSync(teiSimplePrintODD, "utf8")
+//     const simpleDOM = new JSDOM(teiSimpleXML, { contentType: "text/xml" })
+//     const xmlDoc = simpleDOM.window.document;
+
+//     // really need xpath here
+//     const specGroups = xmlDoc.getElementsByTagName('specGrp');
+//     const specEls = specGroups[4].childNodes;
+//     const moduleNames = []
+
+//     for (let i = 0; i < specEls.length; i++) {
+//         const specEl = specEls[i]
+//         if( specEl.localName === 'elementRef') {
+//             moduleNames.push( specEl.getAttribute('key') )    
+//         }
+//     }
+
+//     return moduleNames
+// }
+
+function load(elementIdents) {
+    const specs = {}
+
+    // recursively load module dependencies
+    // assumes 1-1 spec files to specifications
+    const loadSpec = (specName) => {
+        if( !specs[specName] ) {
+            const specPath = `${teiSpecsDir}/${specName}.xml`
+            const moduleSpecs = parseSpecs(specPath)
+            for( const moduleSpec of moduleSpecs ) {
+                specs[moduleSpec.ident] = moduleSpec
+                if( moduleSpec.memberships ) {
+                    for( const membership of moduleSpec.memberships ) {
+                        loadSpec(membership)
+                    }                    
+                }
+                // TODO resolve attrRefs
+            }
+        }
+    }
+
+    // load the element specs and their dependencies
+    for( const elementIdent of elementIdents ) {
+        loadSpec(elementIdent)
+    }
+
+    return specs
+}
+
+function createPhraseElements(specs) {
+    const phraseElements = []
+    for( let phraseMark of phraseMarks) {
+        phraseElements.push({
+            name: phraseMark,
+            pmType: "mark",
+            defaultAttrs: [],
+            desc: specs[phraseMark].description
+        })
+    }
+    return phraseElements
+}
+
+function createDramaElements(specs) {
+    return [{
+        "name": "sp",
+        "pmType": "node",
+        "content": "chunk*",
+        "group": "block",
+        "defaultAttrs": [],
+        "desc": specs['sp'].description
+    }]
+}
+
+function createExamplars(specs) {
+    return [
+        {
+            "name": "div",
+            "pmType": "node",
+            "content": "(chunk|block)*",
+            "group": "block",
+            "defaultAttrs": [],
+            "desc": specs['div'].description
+        },
+        {
+            "name": "p",
+            "pmType": "node",
+            "content": "inline*",
+            "group": "chunk",
+            "defaultAttrs": [],
+            "desc": "marks paragraphs in prose."
+        },
+        {
+            "name": "pb",
+            "pmType": "inline-node",
+            "defaultAttrs": [], // ["facs"],  // TODO remove
+            "desc": "marks the beginning of a new page in a paginated document."
+        },
+        {
+            "name": "note",
+            "pmType": "inline-node",
+            "defaultAttrs": ["id"], // TODO remove
+            "desc": "contains a note or annotation."
+        }
+    ]
+}
+
+function createAttributes( elements, specs ) {
+
+    // for each element, add the attrs to its list of possible attrs
+    const findAttrs = (specIdent) => {
+        const elSpec = specs[specIdent]
+        let elementAttrs = elSpec.attrs ? [ ...elSpec.attrs ] : []
+
+        if( elSpec.memberships ) {
+            for( const membership of elSpec.memberships ) {                
+                elementAttrs = elementAttrs.concat( findAttrs( membership ) )
+            }    
+        }
+        return elementAttrs
+    }
+
+    const attrDefs = {}
+
+    // create a global dictionary of attr definitions and record attrs for each element
+    for( const element of elements ) {
+        const attrs = findAttrs(element.name)
+        for( const attr of attrs ) {
+            if( !attrDefs[attr.ident] ) {
+                attrDefs[attr.ident] = attr
+            }
+            element.defaultAttrs.push( attr.ident )
+        }
+    }
+
+    // convert attr definitions into FairCopy data format
+    const attrs = {}
+    for( const attr of Object.values(attrDefs) ) {
+        attrs[attr.ident] = {
+            type: 'text'
+        }
+    }
+
+    // TODO Remove
+    attrs['id'] = { type: 'text' }
+    // attrs['facs'] = { type: 'text' }
+
+    return attrs
+}
+
+async function run() {
+    const specs = load([ ...phraseMarks, ...examplarEls, ...dramaEls ])
+
+    const elements = []
+
+    elements.push(...createExamplars(specs))
+    elements.push(...createPhraseElements(specs))
+    elements.push(...createDramaElements(specs))
+
+    const attrs = createAttributes(elements,specs)
+
+    const vocabs = {
+        "*[rend]": ["bold","italic","caps"],
+        "name[type]": ["person","place","artwork"]
+    }
+
+    const teiSimpleConfig = { elements, attrs, vocabs }
+    fs.writeFileSync("config/tei-simple.json",JSON.stringify(teiSimpleConfig, null, '\t'))
+}
+
+// A wise turtle that understands ODD and ProseMirror
+
+function main() {
+    run().then(() => {
+        console.log('Done!')
+    }, (err) => {
+        console.log(`${err}: ${err.stack}`)  
+    });
+}
+
+///// RUN THE SCRIPT
+main()
---
+++ scripts/turtle/parse-specs.js
@@ -0,0 +1,134 @@
+const fs = require('fs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+// This module knows how to parse ODD XML specs and definitions into useful JSON objects
+
+const getKeys = (el,keyTag) => {
+    const keys = []
+    const tags = el ? el.getElementsByTagName(keyTag) : []
+
+    for (let i = 0; i < tags.length; i++) {
+        const tagEl = tags[i]
+        if( keyTag === tagEl.localName )  {
+            keys.push( tagEl.getAttribute('key') )    
+        }
+    } 
+    return keys   
+}
+
+function loadLocalizedString(xmlDoc, tagName) {
+    // Load the description of this element
+    let str = ""
+    const descEls = xmlDoc.getElementsByTagName(tagName)
+    for( let i=0; i < descEls.length; i++ ) {
+        const strEl = descEls[i]
+        if( strEl.getAttribute("xml:lang") === "en" ) {
+            // TODO flatten out newlines and runs of whitespace
+            str = strEl.innerHTML
+        }
+    }
+    return str
+}
+
+function parseClassSpec( el ) {
+    const ident = el.getAttribute('ident')
+    const classesEl = el.getElementsByTagName('classes')[0]
+    const memberships = getKeys(classesEl,'memberOf')
+    const attListEl = el.getElementsByTagName('attList')[0]
+    const attrs = attListEl ? parseAttList(attListEl) : []
+    return { ident, memberships, attrs }
+}
+
+function parseAttDef( el ) {
+    const ident = el.getAttribute('ident')
+    const description = loadLocalizedString(el, "desc")
+    const gloss = loadLocalizedString(el, "gloss")
+   
+    return { ident, description, gloss } //, usage , datatype, defaultVal, valList, valDesc }
+}
+
+function parseAttList( el ) {    
+    const attList = []
+    const attDefEls = el.getElementsByTagName('attDef')
+    for( let i=0; i < attDefEls.length; i++ ) {
+        const attDefEl = attDefEls[i]
+        attList.push( parseAttDef(attDefEl) )
+    }
+    // for refs, just return a string
+    const attRefEls = el.getElementsByTagName('attRef')
+    for( let i=0; i < attRefEls.length; i++ ) {
+        const attRefEl = attRefEls[i]
+        attList.push( attRefEl )
+    }
+    
+    // TODO attList - collapse nested attLists.. do these exist? 
+
+    return attList
+}
+
+function parseElementSpec( el ) {
+    const ident = el.getAttribute('ident')
+    const classesEl = el.getElementsByTagName('classes')[0]
+    const memberships = getKeys(classesEl,'memberOf')
+    const description = loadLocalizedString(el, "desc")
+    const gloss = loadLocalizedString(el, "gloss")
+    const attListEl = el.getElementsByTagName('attList')[0]
+    const attrs = attListEl ? parseAttList(attListEl) : []
+
+    // const contentEl = el.getElementsByTagName('content')[0]
+    // const refs = [ 
+    //     ...getKeys(contentEl,'classRef'),
+    //     ...getKeys(contentEl,'elementRef'),
+    //     ...getKeys(contentEl,'macroRef') 
+    // ]
+
+    return { ident, gloss, memberships, description, attrs }
+}
+
+function parseMacroSpec( el ) {
+    const ident = el.getAttribute('ident')
+    return { ident }
+}
+
+function parseDataSpec( el ) {
+    const ident = el.getAttribute('ident')
+    return { ident }
+}
+
+const parseSpecs = function parseSpecs( specPath ) {
+    const specXML = fs.readFileSync(specPath, "utf8")
+    const specDOM = new JSDOM(specXML, { contentType: "text/xml" })
+    const xmlDoc = specDOM.window.document;
+
+    // Is this a class spec, an element spec, a macroSpec, or a dataSpec, some combination?
+    const classSpecEls = xmlDoc.getElementsByTagName('classSpec')
+    const elementSpecEls = xmlDoc.getElementsByTagName('elementSpec')
+    const macroSpecEls = xmlDoc.getElementsByTagName('macroSpec')
+    const dataSpecEls = xmlDoc.getElementsByTagName('dataSpec')
+
+    const classSpecs = [], elementSpecs = [], macroSpecs = [], dataSpecs = []
+
+   // pull all the objects of each type and parse them
+   for( let i=0; i < classSpecEls.length; i++ ) {
+        const classSpecEl = classSpecEls[i]
+        classSpecs.push( parseClassSpec(classSpecEl) )
+    }
+    for( let i=0; i < elementSpecEls.length; i++ ) {
+        const elementSpecEl = elementSpecEls[i]
+        elementSpecs.push( parseElementSpec(elementSpecEl) )
+    }
+    for( let i=0; i < macroSpecEls.length; i++ ) {
+        const macroSpecEl = macroSpecEls[i]
+        macroSpecs.push( parseMacroSpec(macroSpecEl) )
+    }
+    for( let i=0; i < dataSpecEls.length; i++ ) {
+        const dataSpecEl = dataSpecEls[i]
+        dataSpecs.push( parseDataSpec(dataSpecEl) )
+    }
+
+    return [ ...classSpecs, ...elementSpecs, ...macroSpecs, ...dataSpecs ]
+}
+
+// EXPORTS /////////////
+module.exports.parseSpecs = parseSpecs;
