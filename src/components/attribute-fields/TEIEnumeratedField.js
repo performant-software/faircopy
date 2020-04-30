5eca49340aacb734e9d675ea9a8332ce474e53e7
Nick Laiacona <nick@performantsoftware.com>
2020-04-29 20:10:17 -0400

    refactor and debug

--- src/components/attribute-fields/TEIEnumeratedField.js
+++ src/components/attribute-fields/TEIEnumeratedField.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { TextField, IconButton } from '@material-ui/core'
+import React, { Component } from 'react'
+import { TextField, IconButton, Chip } from '@material-ui/core'
 import { Autocomplete } from '@material-ui/lab'
 
 import { teiDataWordValidator } from '../../tei-document/attribute-validators'
@@ -9,59 +9,111 @@ export default class TEIEnumeratedField extends Component {
     constructor(props) {
         super()
         const { value, minOccurs, maxOccurs } = props
-        this.state = value !== "" && value !== null ? teiDataWordValidator(value, minOccurs, maxOccurs) : {}
+        this.state = value && value !== "" ? teiDataWordValidator(value, minOccurs, maxOccurs) : {}
     }
 
-    onChange = (e, value) => {
-        const { onChangeCallback } = this.props
-        let val = value ? value[0] : ''
-        onChangeCallback(val,false)
-    }
+    renderVocabButton() {
+        const { valListType } = this.props
+        
+        if( valListType === 'closed' ) return null
 
-    openVocabEditor = () => {
-        const { elementName, attrName, vocabEditorCallback } = this.props
-        vocabEditorCallback(elementName,attrName)
-    }
-
-    render() {
-        const { elementName, attrName, value, vocab, valListType } = this.props
-        const { error, errorMessage } = this.state
-        const helperText = (errorMessage && errorMessage.length > 0 ) ? errorMessage : " "
-        const valObj = vocab.find( v => v[0] === value )
-        const key = `enumfield-${elementName}-${attrName}-${Date.now()}`
-
-        let vocabButton = null
-        if( valListType !== 'closed' ) {
-            vocabButton = <IconButton 
-                                onClick={this.openVocabEditor} 
-                                tooltip={"Open Vocab Editor"}
-                            >
-                                <i className="fas fa-sm fa-list-alt"></i>
-                            </IconButton>
+        const onClick = () => {
+            const { elementName, attrName, vocabEditorCallback } = this.props
+            vocabEditorCallback(elementName,attrName)
         }
-
-        const renderInput = (params) => <TextField
-                                            {...params}
-                                            label={attrName}
-                                            fullWidth={true}
-                                            error={error}
-                                            helperText={helperText}
-                                        />
-
+    
         return (
-            <div style={{ display: 'flex'}}>
-                <Autocomplete
-                    key={key}
-                    value={valObj}
-                    options={vocab}
-                    onChange={this.onChange}
-                    getOptionLabel={(option) => option[0]}
-                    renderInput={renderInput}
-                    style={{ width: 300 }}
-                />   
-                { vocabButton } 
-            </div>
+            <IconButton 
+                onClick={onClick} 
+                tooltip={"Open Vocab Editor"}
+            >
+                <i className="fas fa-sm fa-list-alt"></i>
+            </IconButton>
         )
     }
 
+    renderInput = (params) => {
+        const { error, errorMessage } = this.state
+        const { attrName } = this.props
+        const helperText = (errorMessage && errorMessage.length > 0 ) ? errorMessage : " "
+
+        return (
+            <TextField
+                {...params}
+                label={attrName}
+                fullWidth={true}
+                error={error}
+                helperText={helperText}
+            />
+        )
+    }
+
+    renderSingleTermField() {
+        const { vocab, value } = this.props
+        const valObj = vocab.find( v => v[0] === value )
+        const key = `singleterm-${Date.now()}`
+
+        const onChange = (e, value) => {
+            const { onChangeCallback } = this.props
+            let val = value ? value[0] : ''
+            onChangeCallback(val,false)
+        }
+
+        return (
+            <Autocomplete
+                key={key}
+                value={valObj}
+                options={vocab}
+                onChange={onChange}
+                getOptionLabel={(option) => option[0]}
+                renderInput={this.renderInput}
+                style={{ width: 300 }}
+            />   
+        )    
+    }
+
+    renderMultiTermField() {
+
+        const onChange = (e, values) => {
+            const { onChangeCallback } = this.props
+            const str = values.join(' ')
+            onChangeCallback(str,false)
+        }
+
+        const renderTags = (values, getTagProps) => {
+            return values.map((option, index) => (
+                <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+            ))
+        }
+
+        const { vocab, value } = this.props
+        const terms = vocab.map( v => v[0] )
+        const values = value.length > 0 ? value.split(' ') : []
+        const key = `multiterm-${Date.now()}`
+
+        return (
+            <Autocomplete
+                multiple
+                key={key}
+                value={values}
+                options={terms}
+                onChange={onChange}
+                getOptionLabel={(option) => option}
+                renderInput={this.renderInput}
+                renderTags={renderTags}
+                style={{ width: 300 }}
+            />   
+        )  
+    }
+
+    render() {
+        const { maxOccurs } = this.props
+
+        return (
+            <div style={{ display: 'flex' }}>
+                { ( maxOccurs ) ? this.renderMultiTermField() : this.renderSingleTermField() }
+                { this.renderVocabButton() } 
+            </div>
+        )
+    }
 }
