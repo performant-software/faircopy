bcb2d7e63dc30395d077f0e7ca4068658834e456
Nick Laiacona <nick@performantsoftware.com>
2020-05-19 09:34:26 -0400

    dry up

--- src/components/ElementMenu.js
+++ src/components/ElementMenu.js
@@ -10,6 +10,39 @@ export default class ElementMenu extends Component {
         }
     }
 
+    renderMenu( menuID, menuItems, subMenu, anchorEl, onClose ) {
+
+        if( !anchorEl ) return null
+
+        let placement, elevation
+        if( subMenu ) {
+            placement = 'right-start'
+            elevation = 12
+        } else {
+            placement = 'bottom'
+            elevation = 6
+        }
+
+        return (
+            <Popper  className="element-menu-popup" placement={placement} open={true} anchorEl={anchorEl} role={undefined} transition disablePortal>
+                {({ TransitionProps, placement }) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                    >
+                    <Paper elevation={elevation}>
+                        <ClickAwayListener onClickAway={onClose}>
+                            <MenuList id={menuID}>
+                                { menuItems }
+                            </MenuList>
+                        </ClickAwayListener>
+                    </Paper>
+                    </Grow>
+                )}
+            </Popper>
+        )
+    }
+
     renderSubMenu() {
         const { subMenu } = this.state
 
@@ -20,26 +53,9 @@ export default class ElementMenu extends Component {
             this.setState({...this.state, subMenu: false })
         }
 
-        return (
-            <Popper  className="element-menu-popup" open={true} anchorEl={this.subMenuEl} role={undefined} transition disablePortal>
-                {({ TransitionProps, placement }) => (
-                    <Grow
-                        {...TransitionProps}
-                        style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-                    >
-                    <Paper>
-                        <ClickAwayListener onClickAway={onClose}>
-                            <MenuList id="submenu-list-grow">
-                                <MenuItem onClick={onClose}>Sub 1</MenuItem>
-                                <MenuItem onClick={onClose}>Sub 2</MenuItem>
-                                <MenuItem onClick={onClose}>Sub 3</MenuItem>
-                            </MenuList>
-                        </ClickAwayListener>
-                    </Paper>
-                    </Grow>
-                )}
-            </Popper>
-        )
+        const menuItems = [ <MenuItem key="sub-menu-1" onClick={onClose}>Sub 1</MenuItem> ]
+
+        return this.renderMenu( 'submenu', menuItems, true, this.subMenuEl, onClose)
     }
 
     render() {
@@ -49,26 +65,15 @@ export default class ElementMenu extends Component {
 
         const openSubMenu = () => { this.setState({...this.state, subMenu: true })}
 
+        const menuItems = [
+            <MenuItem key="menu-1" ref={(el)=> { this.subMenuEl = el }} onClick={openSubMenu}>Profile <i className="fas fa-chevron-right"></i></MenuItem>,
+            <MenuItem key="menu-2" onClick={onClose}>My account</MenuItem>,
+            <MenuItem key="menu-3" onClick={onClose}>Logout</MenuItem>    
+        ]
+
         return (
             <div id="ElementMenu">
-                <Popper  className="element-menu-popup" open={true} anchorEl={anchorEl} role={undefined} transition disablePortal>
-                    {({ TransitionProps, placement }) => (
-                        <Grow
-                            {...TransitionProps}
-                            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-                        >
-                        <Paper>
-                            <ClickAwayListener onClickAway={onClose}>
-                                <MenuList id="menu-list-grow">
-                                    <MenuItem ref={(el)=> { this.subMenuEl = el }} onClick={openSubMenu}>Profile <i className="fas fa-chevron-right"></i></MenuItem>
-                                    <MenuItem onClick={onClose}>My account</MenuItem>
-                                    <MenuItem onClick={onClose}>Logout</MenuItem>
-                                </MenuList>
-                            </ClickAwayListener>
-                        </Paper>
-                        </Grow>
-                    )}
-                </Popper>
+                { this.renderMenu( 'menu', menuItems, false, anchorEl, onClose) }
                 { this.renderSubMenu() }
             </div>
         )
