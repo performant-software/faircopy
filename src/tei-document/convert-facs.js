5f7e7224d5625faa7478867287713e0126695622
Nick Laiacona <nick@performantsoftware.com>
2020-07-26 12:16:32 -0400

    improve parsing for iiif v3

--- src/tei-document/convert-facs.js
+++ src/tei-document/convert-facs.js
@@ -1,23 +1,34 @@
 import {facsTemplate} from "./tei-template"
 
+// Supports IIIF v2 and v3
 export function iiifToFacsimile( manifestData ) {
     const { sequences } = manifestData
-    const manifestID = manifestData['@id']
+    const manifestID = val('id', manifestData)
     const sequence = sequences[0]
     const { canvases } = sequence
 
-    let n=1
+    const surfaceIDs = []
     const surfaces = []
+    let n=1
     for( const canvas of canvases ) {
-        const id = generateID('page-',n++)
         const { images, width, height } = canvas
-        const canvasURI = canvas['@id']
+        const canvasURI = val('id', canvas)
         const image = images[0]
         const { resource } = image
-        const imageAPIURL = resource.service ? resource.service['@id'] : resource['@id']
+        const imageAPIURL = resource.service ? val('id', resource.service) : val('id', resource)
+        const labels = str( 'label', canvas, 'en' )
+        const label = labels && labels[0] ? labels[0] : ""
+        let id = getIDfromURI(canvasURI)      
+        // fallback to page number if we can't create a unique ID from canvas ID 
+        if( !id || surfaceIDs.includes(id) ) {
+            id = generateOrdinalID('page-',n)
+        }  
+        surfaceIDs.push(id)
+        n++ // page count
 
         surfaces.push({
             id,
+            label,
             width,
             height,
             imageAPIURL,
@@ -67,7 +78,13 @@ export function facsimileToTEI(facs) {
    return facsTemplate(facs)
 }
 
-function generateID( prefix, ordinalID ) {
+function getIDfromURI(uri) {
+    const parts = uri.split('/')
+    const lastIndex = parts.length - 1
+    return parts[lastIndex]
+}
+
+function generateOrdinalID( prefix, ordinalID ) {
     let zeros = ""
 
     if( ordinalID < 10 ) {
@@ -79,4 +96,52 @@ function generateID( prefix, ordinalID ) {
     }
 
     return `${prefix}${zeros}${ordinalID}`
+}
+
+// For string value compliance with IIIF v3 (see: https://iiif.io/api/presentation/3.0/#44-language-of-property-values)
+function str( key, obj, lang ) {
+    const values = obj[key]
+    // IIIF v2 doesn't have localized values
+    if( typeof values === 'string' ) {
+        return [ values ]
+    } else {
+        const langKeys = Object.keys(values)
+
+        // No values provided
+        if( langKeys.length === 0 ) return []
+
+        // If all of the values are associated with the none key, the client must display all of those values.
+        if( langKeys.includes('none') && langKeys.length === 1) {
+            return values['none']
+        }
+        // If any of the values have a language associated with them, the client must display all of the values associated with the language that best matches the language preference.
+        if( values[lang] ) {
+            return values[lang]
+        } 
+        if( !langKeys.includes('none') ) {
+            // If all of the values have a language associated with them, and none match the language preference, the client must select a language and display all of the values associated with that language.
+            return values['en'] ? values['en'] : values[langKeys[0]]
+        } else {
+            // If some of the values have a language associated with them, but none match the language preference, the client must display all of the values that do not have a language associated with them.
+            return values['none']
+        }
+    }
+}
+
+const JSONLDKeywords = ['id', 'type', 'none']
+
+// JSON-LD keywords in IIIF v3 do not have @ symbols
+function val( key, obj ) {
+    if( JSONLDKeywords.includes(key) ) {
+        const atKey = `@${key}`
+        if( obj[atKey] ) {
+            return obj[atKey]
+        } else if( obj[key] ) {
+            return obj[key]
+        } else {
+            return undefined
+        }    
+    } else {
+        return obj[key]
+    }
 }
