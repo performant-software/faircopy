d2adc42b55983959f7fef544b966f67d5cafcf06
Nick Laiacona <nick@performantsoftware.com>
2020-09-03 15:51:38 -0400

    add support for IIIF v3, better error handling

--- src/tei-document/convert-facs.js
+++ src/tei-document/convert-facs.js
@@ -2,6 +2,77 @@ import {facsTemplate} from "./tei-template"
 
 // Supports IIIF v2 and v3
 export function iiifToFacsimile( manifestData ) {
+    const context = manifestData["@context"]
+    if( context.includes("http://iiif.io/api/presentation/3/context.json") ) {
+        return iiifToFacsimile3( manifestData )
+    }
+    if( context.includes("http://iiif.io/api/presentation/2/context.json") ) {
+        return iiifToFacsimile2( manifestData )
+    }
+    throw new Error("Expected IIIF Presentation API context 2 or 3.")
+}
+
+function iiifToFacsimile3( manifestData ) {
+    if( manifestData.type !== "Manifest" ) throw new Error("Expected a manifest as the root object.")
+
+    const canvases = manifestData.items
+    const manifestID = val('id', manifestData)
+
+    const surfaceIDs = []
+    const surfaces = []
+    let n=1
+    for( const canvas of canvases ) {
+        if( canvas.type !== "Canvas" ) throw new Error("Expected a Canvas item.")
+        const canvasURI = canvas.id
+        const annotationPage = canvas.items[0]
+        if( !annotationPage || annotationPage.type !== "AnnotationPage" ) throw new Error("Expected an Annotation Page item.")
+        const annotations = annotationPage.items
+        for( const annotation of annotations ) {
+            if( annotation.type !== "Annotation" ) throw new Error("Expected an Annotation item.")
+            if( annotation.motivation === "painting" && annotation.body && annotation.body.type === "Image" ) {
+                const {body} = annotation
+                const {width,height} = body
+                let imageAPIURL
+                if( body.service ) {
+                    for( const serving of body.service ) {
+                        const servingType = val('type', serving)
+                        if( servingType === "ImageService2") {
+                            imageAPIURL = val('id', serving)
+                            break
+                        }
+                    }
+                } else {
+                    imageAPIURL = val('id', body)
+                }
+                let localLabels = str( canvas.label )
+                let id = getIDfromURI(canvasURI)
+                if( !id || surfaceIDs.includes(id) ) {
+                    id = generateOrdinalID('page-',n)
+                }
+                localLabels = !localLabels ? id : localLabels
+                surfaceIDs.push(id)
+                n++ // page count
+
+                surfaces.push({
+                    id,
+                    localLabels,
+                    width,
+                    height,
+                    imageAPIURL,
+                    canvasURI
+                })  
+                break // one surface per canvas      
+            }
+        } 
+    }
+
+    return {
+        manifestID,
+        surfaces
+    }
+}
+
+function iiifToFacsimile2( manifestData ) {
     const { sequences } = manifestData
     const manifestID = val('id', manifestData)
     const sequence = sequences[0]
