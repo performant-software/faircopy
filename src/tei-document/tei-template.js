fb0cc4b7dd6b266cc3ff99c12cccadfaf46bc7e8
Nick Laiacona <nick@performantsoftware.com>
2020-10-08 08:41:54 -0400

    start blank docs with ab and textnode

--- src/tei-document/tei-template.js
+++ src/tei-document/tei-template.js
@@ -21,7 +21,7 @@ export const teiTemplate = `<?xml version="1.0" encoding="UTF-8"?>
         </fileDesc>
     </teiHeader>
 <text>
-    <body></body>
+    <body><ab><textNode></textNode></ab></body>
 </text>
 </TEI>
 `
