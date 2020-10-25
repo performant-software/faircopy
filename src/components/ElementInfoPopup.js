511405f503dc48e5e815cf35c1079c9f9c0ed01f
Nick Laiacona <nick@performantsoftware.com>
2020-10-25 09:39:14 -0400

    account for all elements

--- src/components/ElementInfoPopup.js
+++ src/components/ElementInfoPopup.js
@@ -15,18 +15,20 @@ export default class ElementInfoPopup extends Component {
                         <Typography variant="body2" component="p">{desc}</Typography><br/>
                         { (containedBy||mayContain||notes) && 
                             <table className="rules-table">
-                                { containedBy && <tr>
-                                    <td><Typography variant="body2" component="span">Contained By:</Typography></td>
-                                    <td><Typography variant="body2" component="span">{containedBy}</Typography></td>
-                                </tr> }
-                                { mayContain && <tr>
-                                    <td><Typography variant="body2" component="span">May Contain:</Typography></td>
-                                    <td><Typography variant="body2" component="span">{mayContain}</Typography></td>
-                                </tr> }
-                                { notes && <tr>
-                                    <td><Typography variant="body2" component="span">Notes:</Typography></td>
-                                        <td><Typography variant="body2" component="span">{notes}</Typography></td>
-                                </tr> }
+                                <tbody>
+                                    { containedBy && <tr>
+                                        <td><Typography variant="body2" component="span">Contained By:</Typography></td>
+                                        <td><Typography variant="body2" component="span">{containedBy}</Typography></td>
+                                    </tr> }
+                                    { mayContain && <tr>
+                                        <td><Typography variant="body2" component="span">May Contain:</Typography></td>
+                                        <td><Typography variant="body2" component="span">{mayContain}</Typography></td>
+                                    </tr> }
+                                    { notes && <tr>
+                                        <td><Typography variant="body2" component="span">Notes:</Typography></td>
+                                            <td><Typography variant="body2" component="span">{notes}</Typography></td>
+                                    </tr> }
+                                </tbody>
                             </table>
                         }
                     </CardContent>
