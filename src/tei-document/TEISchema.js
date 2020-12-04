3d84aaf9697561df3e0dc5f4079e0e2de703e67b
Nick Laiacona <nick@performantsoftware.com>
2020-12-04 11:28:30 -0500

    normalize whitespace

--- src/tei-document/TEISchema.js
+++ src/tei-document/TEISchema.js
@@ -3,6 +3,9 @@ import {DOMParser as PMDOMParser } from "prosemirror-model"
 import {DOMSerializer} from "prosemirror-model"
 import { NodeRange } from 'prosemirror-model'
 
+// These elements are processed in XSLT by the xsl:strip-space command. This list is from xml/tei/odd/stripspace.xsl.model
+const xmlStripSpaceNames = "TEI abstract additional address adminInfo altGrp altIdentifier alternate analytic annotation annotationBlock app appInfo application arc argument att attDef attList availability back biblFull biblStruct bicond binding bindingDesc body broadcast cRefPattern calendar calendarDesc castGroup castList category certainty char charDecl charProp choice cit classDecl classSpec classes climate cond constraintSpec content correction correspAction correspContext correspDesc custodialHist dataRef dataSpec datatype decoDesc dimensions div div1 div2 div3 div4 div5 div6 div7 divGen docTitle eLeaf eTree editionStmt editorialDecl elementSpec encodingDesc entry epigraph epilogue equipment event exemplum fDecl fLib facsimile figure fileDesc floatingText forest front fs fsConstraints fsDecl fsdDecl fvLib gap gi glyph graph graphic group handDesc handNotes history hom hyphenation iNode if imprint incident index interpGrp interpretation join joinGrp keywords kinesic langKnowledge langUsage layoutDesc leaf lg linkGrp list listAnnotation listApp listBibl listChange listEvent listForest listNym listObject listOrg listPerson listPlace listPrefixDef listRef listRelation listTranspose listWit location locusGrp macroSpec media metDecl model modelGrp modelSequence moduleRef moduleSpec monogr msContents msDesc msFrag msIdentifier msItem msItemStruct msPart namespace node normalization notatedMusic notesStmt nym object objectDesc objectIdentifier org paramList paramSpec particDesc performance person personGrp persona physDesc place population postscript precision prefixDef profileDesc projectDesc prologue publicationStmt punctuation quotation rdgGrp recordHist recording recordingStmt refsDecl relatedItem relation remarks respStmt respons revisionDesc root row samplingDecl schemaRef schemaSpec scriptDesc scriptStmt seal sealDesc segmentation sequence seriesStmt set setting settingDesc sourceDesc sourceDoc sp spGrp space spanGrp specGrp specList standOff state stdVals styleDefDecl subst substJoin superEntry supportDesc surface surfaceGrp table tagsDecl taxonomy teiCorpus teiHeader terrain text textClass textDesc timeline titlePage titleStmt trait transcriptionDesc transpose tree triangle typeDesc unitDecl unitDef vAlt vColl vDefault vLabel vMerge vNot vRange valItem valList vocal".split(' ')
+
 export default class TEISchema {
 
     constructor(json) {
@@ -42,6 +45,28 @@ export default class TEISchema {
                     interEl.innerHTML = markEl.innerHTML
                     markEl.parentNode.replaceChild(interEl,markEl)
                 }
+            }
+        }
+
+        // Normalize the whitespace in the incoming text
+        for( const xmlStripSpaceName of xmlStripSpaceNames ) {
+            const stripEls = textEl.querySelectorAll(xmlStripSpaceName)
+            for( let i=0; i < stripEls.length; i++ ) {
+                const stripEl = stripEls[i]
+                // look at each child, if it contains only whitespace, remove it
+                const nextEl = document.createElement(stripEl.nodeName)
+                for( let j=0; j < stripEl.childNodes.length; j++ ) {
+                    const child = stripEl.childNodes[j]
+                    if( child.nodeName === '#text' ) {
+                        const nextContent = child.textContent.replace(/\s+/g, '')
+                        if( nextContent.length > 0 ) {                            
+                            nextEl.appendChild(nextContent)
+                        }
+                    } else {
+                        nextEl.appendChild(child)
+                    }
+                }
+                stripEl.parentNode.replaceChild(nextEl,stripEl)
             }
         }
 
