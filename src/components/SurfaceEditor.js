b47645c005dd6b2de2df7e19d601b9dac98ae760
Nick Laiacona <nick@performantsoftware.com>
2021-01-19 13:53:11 -0500

    add fabric overlay

--- src/components/SurfaceEditor.js
+++ src/components/SurfaceEditor.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import { Typography } from '@material-ui/core'
-import OpenSeadragon from 'openseadragon';
+import OpenSeadragon from 'openseadragon-fabricjs-overlay/openseadragon/openseadragon';
+import { fabric } from 'openseadragon-fabricjs-overlay/fabric/fabric.adapted';
+import openSeaDragonFabricOverlay from 'openseadragon-fabricjs-overlay/openseadragon-fabricjs-overlay';
 
 import { getImageInfoURL } from '../tei-document/iiif'
 import SurfaceEditorToolbar from './SurfaceEditorToolbar'
 import SurfaceDetailCard from './SurfaceDetailCard'
-import SeaDragonComponent from './SeaDragonComponent'
+
+// overlay these modules
+openSeaDragonFabricOverlay(OpenSeadragon, fabric);
+
+// from https://stackoverflow.com/a/48343346/6126327 - show consistent stroke width regardless of object scaling
+fabric.Object.prototype._renderStroke = function(ctx) {
+    if (!this.stroke || this.strokeWidth === 0) {
+        return;
+    }
+    if (this.shadow && !this.shadow.affectStroke) {
+        this._removeShadow(ctx);
+    }
+    ctx.save();
+    ctx.scale(1 / this.scaleX, 1 / this.scaleY);
+    this._setLineDash(ctx, this.strokeDashArray, this._renderDashedStroke);
+    this._applyPatternGradientTransform(ctx, this.stroke);
+    ctx.stroke();
+    ctx.restore();
+};
 
 export default class SurfaceEditor extends Component {
 
@@ -19,35 +39,33 @@ export default class SurfaceEditor extends Component {
     initViewer = (el) => {
         if( !el ) {
             this.viewer = null
+            this.overlay = null
             return
         }
         const { facsDocument, surfaceIndex } = this.props
         const surface = facsDocument.getSurface(surfaceIndex)
 
+        const createOSD = (tileSource) => {
+            this.viewer = OpenSeadragon({
+                element: el,
+                tileSources: tileSource,
+                showHomeControl: false,
+                showFullPageControl: false,
+                showZoomControl: false
+            })    
+            this.overlay = this.viewer.fabricjsOverlay({scale: 2000});
+        }
+
         if( surface.type === 'iiif') {
             const imageInfoURL = getImageInfoURL( surface )
             axios.get(imageInfoURL).then((response) => {
-                const tileSource = response.data
-                this.viewer = OpenSeadragon({
-                    element: el,
-                    tileSources: tileSource,
-                    showHomeControl: false,
-                    showFullPageControl: false,
-                    showZoomControl: false
-                })    
+                createOSD(response.data)   
             }, (err) => {
                 console.log('Unable to load image: ' + err);
             })    
         } else {
             const imageFileURL = `local://${surface.resourceEntryID}`
-            this.viewer = OpenSeadragon({
-                element: el,
-                tileSources: { type: 'image', url: imageFileURL },
-                showHomeControl: false,
-                showFullPageControl: false,
-                maxZoomPixelRatio: Infinity,
-                showZoomControl: false
-            }) 
+            createOSD({ type: 'image', url: imageFileURL })
         }
     }
     
@@ -92,3 +110,17 @@ export default class SurfaceEditor extends Component {
         )
     }
 }
+
+class SeaDragonComponent extends Component {
+  
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        const { initViewer, showSearchBar } = this.props
+        const searchFlag = showSearchBar ? 'search-on' : 'search-off' 
+        return <div className={`osd-viewer ${searchFlag}`} ref={(el)=> { initViewer(el) }}></div>
+    }
+}
+  
