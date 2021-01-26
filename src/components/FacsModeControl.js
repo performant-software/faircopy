d819f888556ce77d0addb27966abacf3f4504003
Nick Laiacona <nick@performantsoftware.com>
2021-01-26 18:08:33 -0500

    fix mode control layout, add tooltip

--- src/components/FacsModeControl.js
+++ src/components/FacsModeControl.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react'
-import { Button } from '@material-ui/core';
+import { Button, Tooltip } from '@material-ui/core';
 
 export default class FacsModeControl extends Component {    
     render() {
         const { selected, buttonProps, onChangeView, surfaceIndex } = this.props
 
         return (
-            <span id="FacsModeControl" >
-                <Button
-                    className="toolbar-button-right"
-                    {...buttonProps}
-                    onClick={ () => onChangeView(surfaceIndex,'detail') }
-                >
-                    <i className={`${ selected === 'detail' ? 'selected-action' : ''} fas fa-image fa-2x`}></i>
-                </Button> 
-                <Button
-                    className="toolbar-button-right"
-                    {...buttonProps}
-                    onClick={ () => onChangeView(surfaceIndex,'index') }
-                >
-                    <i className={`${ selected === 'index' ? 'selected-action' : ''} fas fa-list fa-2x`}></i>
-                </Button> 
-            </span>
+            <div id="FacsModeControl" >
+                <Tooltip title="Index View">
+                    <span>
+                        <Button
+                            {...buttonProps}
+                            onClick={ () => onChangeView(surfaceIndex,'index') }
+                        >
+                            <i className={`${ selected === 'index' ? 'selected-action' : ''} fas fa-list fa-2x`}></i>
+                        </Button> 
+                    </span>
+                </Tooltip>
+                <Tooltip title="Detail View">
+                    <span>
+                        <Button
+                            {...buttonProps}
+                            onClick={ () => onChangeView(surfaceIndex,'detail') }
+                        >
+                            <i className={`${ selected === 'detail' ? 'selected-action' : ''} fas fa-image fa-2x`}></i>
+                        </Button> 
+                    </span>
+                </Tooltip>
+            </div>
         )
     }
 }
