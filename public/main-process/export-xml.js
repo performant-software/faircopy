0411c589b983a9ba35ff4aed3c02a5b152b974fd
Nick Laiacona <nick@performantsoftware.com>
2021-03-10 08:29:49 -0500

    add xml:id to export

--- public/main-process/export-xml.js
+++ public/main-process/export-xml.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const log = require('electron-log')
 const jsdom = require("jsdom")
 const { JSDOM } = jsdom
+const serialize = require("w3c-xmlserializer");
 
 const exportResources =  async function exportResources(resourceIDs,path,projectStore) {
     for( const resourceID of resourceIDs ) {
@@ -23,7 +24,7 @@ async function exportTEIDoc(resourceEntry,path,projectStore) {
         const resourceEntry = projectStore.manifestData.resources[resourceID]
         const resourceXML = await projectStore.readUTF8File(resourceID)
         const elName = resourceEntry.type === 'header' ? 'teiHeader' : resourceEntry.type === 'text' ? 'text' : 'facsimile'
-        const resourceEl = getResourceEl( resourceXML, elName )
+        const resourceEl = getResourceEl( resourceXML, elName, resourceEntry.localID )
         if( resourceEntry.type === 'header' ) {
             header = resourceEl
         } else {
@@ -31,22 +32,22 @@ async function exportTEIDoc(resourceEntry,path,projectStore) {
         }
     }
 
-    const teiDoc = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <TEI xmlns="http://www.tei-c.org/ns/1.0">
-        ${header}
-        ${resources.join('\n')}
-    </TEI>
-    `
-
-    exportXMLFile(path,resourceEntry.localID,teiDoc)
+    const resourceXML = `<?xml version="1.0" encoding="UTF-8"?><TEI xmlns="http://www.tei-c.org/ns/1.0"></TEI>`
+    const resourceDOM = new JSDOM(resourceXML, { contentType: "text/xml" })
+    const xmlDoc = resourceDOM.window.document
+    const teiDoc = xmlDoc.getElementsByTagName('TEI')[0]
+    teiDoc.appendChild(header)
+    resources.map( resource => teiDoc.appendChild(resource))
+    const teiDocXML = serialize(xmlDoc)
+    exportXMLFile(path,resourceEntry.localID,teiDocXML)
 }
 
-function getResourceEl( resourceXML, elName ) {
+function getResourceEl( resourceXML, elName, localID ) {
     const resourceDOM = new JSDOM(resourceXML, { contentType: "text/xml" })
     const xmlDoc = resourceDOM.window.document
     const el = xmlDoc.getElementsByTagName(elName)[0]
-    return el.outerHTML
+    el.setAttribute('xml:id',localID)
+    return el
 }
 
 async function exportXMLFile(path, localID, content) {
