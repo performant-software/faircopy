d0fdb8b77c394d05dd900fca4fc04ae1ab9f98cc
Nick Laiacona <nick@performantsoftware.com>
2021-05-24 07:43:57 -0400

    refactor settings button

--- src/components/ProjectSidebar.js
+++ src/components/ProjectSidebar.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
-import { Button, Typography } from '@material-ui/core'
+import { IconButton, Typography, Tooltip } from '@material-ui/core'
 import TableOfContents from './TableOfContents'
 import ProjectNavigator from './ProjectNavigator'
 
 export default class ProjectSidebar extends Component {
+
+    renderButton(title,icon,onClick,enabled=true,onRef=null,active=false) {
+        const refProps = onRef ? { ref: onRef } : {}
+        const colorProps = active ? { color: 'primary' } : {}
+        const iconButton = (
+            <IconButton
+                disabled={!enabled}
+                onClick={onClick}
+                {...this.buttonProps}
+                {...refProps}
+                {...colorProps}
+            >
+                <i className={`${icon} fa-sm`}></i>
+            </IconButton> 
+        )
+             
+        return enabled ? <Tooltip title={title}>{iconButton}</Tooltip> : iconButton
+    }
 
     render() {
         const { fairCopyProject, selectedResource, openResources, onSelectResource, onEditProjectInfo, onCloseResource, panelWidth } = this.props
@@ -11,13 +29,7 @@ export default class ProjectSidebar extends Component {
             <div id="ProjectSidebar">
                 <div className="title-bar">
                     <Typography  variant="h6" className="project-title">{fairCopyProject.projectName}</Typography>
-                    <Button className="edit-button"
-                        onClick={onEditProjectInfo}
-                        disableRipple={true}
-                        disableFocusRipple={true}
-                    >
-                        <i className="far fa-edit fa-2x"></i>
-                    </Button>
+                    { this.renderButton('Edit Project Info', 'fas fa-cog', onEditProjectInfo )}
                 </div>
                 <ProjectNavigator
                     openResources={openResources}
