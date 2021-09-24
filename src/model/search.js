aa4d6853f7bb51a1ad1b5aec5cc07b376cef3105
Nick Laiacona <nick@performantsoftware.com>
2021-09-24 09:33:18 -0400

    first pass at search module

--- src/model/search.js
+++ src/model/search.js
@@ -1,103 +1,151 @@
 import lunr from 'lunr';
 
-let nextRecordID = 1
-
 export function createIndex() {
-
-    var searchIndex = lunr(function () {
-        this.pipeline.remove(lunr.stemmer)
-        this.pipeline.remove(lunr.stopWordFilter)
-        this.searchPipeline.remove(lunr.stemmer)
-        this.searchPipeline.remove(lunr.stopWordFilter)
-        this.ref('id')
-        this.field('elementName')
-        this.field('softNode')
-        this.field('attr_xmlID')
-        this.field('attr_rend')
-        this.field('attr_facs')
-        this.field('attr_place')
-        this.field('contents')
-
-        for( const sampleDoc of sampleDocs ) {
-            this.add(sampleDoc)
-        }    
-    })
-
-    return searchIndex
+    return {}
 }
 
 export function loadIndex( indexJSON ) {
     // TODO
 }
 
-export function indexDocument( teiDocument ) {
-    const { fairCopyProject, editorView } = teiDocument
-    const { searchIndex } = fairCopyProject
-    const { doc } = editorView.state
+function getSafeAttrKey( attrName ) {
+    return attrName.replace(':','')
+}
 
-    const id = nextRecordID++
-    const content = doc.textContent
-    const searchDoc = { id, content }
-    searchIndex.add(id, searchDoc)
+function defineAttrFields( lunrIndex, attrs ) {
+    for( const attr of Object.keys(attrs) ) {
+        const attrSafeKey = getSafeAttrKey(attr)
+        const fieldName = `attr_${attrSafeKey}`
+        lunrIndex.field(fieldName)
+    }
+}
+
+function defineLunrSchema( lunrIndex, attrs ) {
+    // configure pipeline for exact matching search, language independent
+    lunrIndex.pipeline.remove(lunr.stemmer)
+    lunrIndex.pipeline.remove(lunr.stopWordFilter)
+    lunrIndex.searchPipeline.remove(lunr.stemmer)
+    lunrIndex.searchPipeline.remove(lunr.stopWordFilter)
+
+    // add fields to schema
+    lunrIndex.ref('locator')
+    lunrIndex.field('elementName')
+    lunrIndex.field('softNode')
+    lunrIndex.field('contents')
+    defineAttrFields( lunrIndex, attrs )
+}
+
+export function indexDocument( teiDocument ) {
+    const { fairCopyProject, resourceID, editorView } = teiDocument
+    const { searchIndex, teiSchema } = fairCopyProject
+    const { elements, attrs } = teiSchema
+    const { doc } = editorView.state
+    
+    searchIndex[resourceID] = lunr( function () {
+        defineLunrSchema(this,attrs)
+
+        doc.descendants((node,pos) => {
+            const locator = `${resourceID}/${pos}`
+            const elementName = node.type.name
+            const contents = node.textContent
+            const element = elements[elementName]
+            if( !element ) return true
+
+            const { fcType } = element
+            const softNode = fcType === 'soft' ? 'true' : 'false'
+            const attrFields = {}
+            
+            for( const attrKey of Object.keys(node.attrs) ) {
+                const attrVal = node.attrs[attrKey]
+                const attrSafeKey = getSafeAttrKey(attrKey)
+                attrFields[`attr_${attrSafeKey}`] = attrVal
+            }
+
+            // TODO index marks
+            // TODO sub docs
+
+            this.add({
+                locator,
+                elementName,
+                softNode,
+                contents,
+                ...attrFields
+            })
+            return true
+        })
+
+    })
 }
 
 export function searchProject( query, searchIndex ) {
-    const results = searchIndex.search('+contents:this +contents:is +elementName:p')
+    const results = {}
+    for( const resourceID of Object.keys(searchIndex) ) {
+        results[resourceID] = searchResource( query, resourceID, searchIndex )
+    }
+
     debugger
 }
 
-/// Sample doc:
+export function searchResource( query, resourceID, searchIndex ) {
+    const resourceIndex = searchIndex[resourceID]
 
-const sampleCodex = [
-    {
-        resourceID: 'sdsds-dsdsd-dsdsd-dsds',
-        subDocID: null,
-        offset: 1,
-        textNodes: []
+    // create a list of terms from the query
+    const terms = query.split(' ')
+    const termQs = []
+    for( const term of terms ) {
+        termQs.push(`+contents:${term}`)
     }
-]
+    const termQ = termQs.join(' ')
+
+    // full text search 
+    // return resourceIndex.search(termQ)
+    return resourceIndex.search(`+softNode:true ${termQ}`)
+
+    // TODO find a element containing a phrase w/certain attrs
+    // const results = searchIndex.search('+contents:this +contents:is +elementName:p +attr_rend:bold')
+}
+
 
 // highlight would look inside each of the text blocks and highlight the search for terms or phrase
 
-const sampleDocs = [
-    {
-        id: 1,
-        elementName: "body",
-        contents: "This is my textNode's deleted content.\nRabbit is the second paragraph."
-    },
-    {
-        id: 2,
-        elementName: "div",
-        attr_xmlID: "foo",
-        contents: "This is my textNode's deleted content.\nThis is the second paragraph."
-    },
-    {
-        id: 3,
-        elementName: "p",
-        softNode: "true",
-        attr_rend: "bold",
-        contents: "This is my textNode's rabbit deleted content."
-    },
-    {
-        id: 4,
-        elementName: "pb",
-        attr_facs: "pics#101"
-    },
-    {
-        id: 5,
-        elementName: "note",
-        softNode: "true",
-        contents: "This is my note."
-    },
-    {
-        id: 6,
-        elementName: "del",
-        attr_place: "above",
-        contents: "deleted"
-    },
-    {
-        id: 7,
-        elementName: "p",
-        contents: "This is the second  rabbit paragraph."
-    }
-]
+// const sampleDocs = [
+//     {
+//         locator: 'sdsds-dsdsd-dsdsd-dsds/1',
+//         elementName: "body",
+//         contents: "This is my textNode's deleted content.\nRabbit is the second paragraph."
+//     },
+//     {
+//         locator: 'sdsds-dsdsd-dsdsd-dsds/2',
+//         attr_xmlID: "foo",
+//         contents: "This is my textNode's deleted content.\nThis is the second paragraph."
+//     },
+//     {
+//         locator: 'sdsds-dsdsd-dsdsd-dsds/10',
+//         elementName: "p",
+//         softNode: "true",
+//         attr_rend: "bold",
+//         contents: "This is my textNode's rabbit deleted content."
+//     },
+//     {
+//         locator: 'sdsds-dsdsd-dsdsd-dsds/20',    
+//         elementName: "pb",
+//         attr_facs: "pics#101"
+//     },
+//     {
+//         locator: 'sdsds-dsdsd-dsdsd-dsds/30',   
+//         elementName: "note",
+//         softNode: "true",
+//         contents: "This is my note."
+//     },
+//     {
+//         locator: 'sdsds-dsdsd-dsdsd-dsds/40',  
+//         elementName: "del",
+//         attr_place: "above",
+//         contents: "deleted"
+//     },
+//     {
+//         locator: 'sdsds-dsdsd-dsdsd-dsds/50', 
+//         elementName: "p",
+//         contents: "This is the second  rabbit paragraph."
+//     }
+// ]
