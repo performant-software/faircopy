c9f3af8727dcc7be634e59c951a4beaf152fd910
Nick Laiacona <nick@performantsoftware.com>
2021-11-03 09:51:59 -0400

    protect against invalid results

--- src/model/search-highlighter.js
+++ src/model/search-highlighter.js
@@ -25,6 +25,7 @@ export function searchHighlighter() {
         const pluginState = plugin.getState(state)
         const { searchQuery, searchResults } = pluginState
         const terms = searchQuery.toLowerCase().split(' ')
+        let resultsInvalid = false
 
         if( searchResults.length > 0 ) {
             const decorations = []
@@ -33,19 +34,26 @@ export function searchHighlighter() {
                 const parentNode = $node.parent
 
                 // find an exact match to the search query and highlight it
+                // eslint-disable-next-line no-loop-func
                 parentNode.descendants( (node,pos) => {
                     if( node.type.name.includes('textNode') ) {
-                        const from = nodePos+pos
-                        const to = from + node.nodeSize
-                        const text = doc.textBetween(from,to, ' ', ' ')
-                        for( const term of terms ) {
-                            const textOffset = text.indexOf(term)
-                            if( textOffset !== -1 ) {
-                                // highlight the matching term
-                                const termFrom = nodePos+pos+textOffset+2
-                                const termTo = termFrom + term.length
-                                decorations.push(Decoration.inline(termFrom, termTo, {style: `background: ${searchHighlightColor}`}))
-                            }
+                        try {
+                            const from = nodePos+pos
+                            const to = from + node.nodeSize
+                            const text = doc.textBetween(from,to, ' ', ' ')
+                            for( const term of terms ) {
+                                const textOffset = text.indexOf(term)
+                                if( textOffset !== -1 ) {
+                                    // highlight the matching term
+                                    const termFrom = nodePos+pos+textOffset+2
+                                    const termTo = termFrom + term.length
+                                    decorations.push(Decoration.inline(termFrom, termTo, {style: `background: ${searchHighlightColor}`}))
+                                }
+                            }    
+                        } catch(e) {
+                            // If the user edits the document, it can invalid the search results by moving the offsets around.
+                            // In this case, clear the highlights.
+                            resultsInvalid = true
                         }
                         return false
                     }
@@ -53,7 +61,11 @@ export function searchHighlighter() {
                 })
             }
 
-            return DecorationSet.create(doc, decorations)
+            if( resultsInvalid ) {
+                return DecorationSet.create(doc, [])
+            } else {
+                return DecorationSet.create(doc, decorations)
+            }            
         }
     }
 
