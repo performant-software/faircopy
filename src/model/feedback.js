6d697e5aa39de166cc1e047e0ad9152e059dcd30
Nick Laiacona <nick@performantsoftware.com>
2022-02-01 16:14:10 -0500

    adjust feedback endpoint

--- src/model/feedback.js
+++ src/model/feedback.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const devFeedbackURL = 'https://faircopy-activate-2-staging.herokuapp.com/feedback'
-const prodFeedbackURL = 'https://faircopyeditor.com/feedback'
+const devFeedbackURL = 'https://faircopy-activate-2-staging.herokuapp.com/api/public/feedback'
+const prodFeedbackURL = 'https://faircopyeditor.com/api/public/feedback'
 
 export function sendFeedback(devMode,message,version,onSuccess,onError) {
 
@@ -12,7 +12,7 @@ export function sendFeedback(devMode,message,version,onSuccess,onError) {
     const feedbackURL = devMode ? devFeedbackURL : prodFeedbackURL
 
     axios.post(feedbackURL, {
-        feedback: { version, licenseKey, message }
+        feedback: { version, license_key: licenseKey, message }
     }).then(
         (resp) => {
             onSuccess()
