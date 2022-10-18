8675243f15f3286040ac2af855c8007e95ac8a7a
Nick Laiacona <nick@performantsoftware.com>
2022-10-18 08:12:24 -0400

    fix thumbnail popup bug

--- src/model/IDMap.js
+++ src/model/IDMap.js
@@ -32,8 +32,9 @@ export default class IDMap {
                     const { resourceType, resourceID, ids } = resourceMap
                     if( resourceType === 'teidoc' ) {
                         for( const childID of Object.keys(ids)) {
-                            if( ids[childID].ids[xmlID] ) {
-                                return { localID: childID, xmlID, resourceID, ...ids[childID].ids[xmlID] }
+                            const child = ids[childID]
+                            if( child.ids[xmlID] ) {
+                                return { localID: childID, xmlID, resourceID: child.resourceID, ...ids[childID].ids[xmlID] }
                             }
                         }
                     } else {
