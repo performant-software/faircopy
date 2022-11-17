7818207d358f8256a9242b28da1a3a9d67650910
Nick Laiacona <nick@performantsoftware.com>
2022-11-17 08:05:53 -0500

    display permissions

--- src/components/project-settings-window/GeneralSettings.js
+++ src/components/project-settings-window/GeneralSettings.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
-import { Typography, TextField, Button } from '@material-ui/core'
+import { Paper, Typography, TextField, Button } from '@material-ui/core'
+import { canCheckOut, canCreate, canDelete, canConfigAdmin } from '../../model/permissions'
 
 export default class GeneralSettings extends Component {
     
@@ -14,15 +15,21 @@ export default class GeneralSettings extends Component {
     }
 
     renderPermissions( permissions ) {
-        const permissionList = permissions.map( permission => {
-            return <li><Typography key={`perm-${permission}`}>{permission}</Typography></li>
-        })
+        const checkout = yesOrNo(canCheckOut(permissions))
+        const create = yesOrNo(canCreate(permissions))
+        const del = yesOrNo(canDelete(permissions))
+        const config = yesOrNo(canConfigAdmin(permissions))
+
         return (
-            <div>
+            <Paper className="permissions-section" elevation={1}>
+                <Typography variant="h5">User Permissions</Typography>
                 <ul>
-                    { permissionList }
+                   <li><Typography>Can Checkout: {checkout}</Typography></li>
+                   <li><Typography>Can Create: {create}</Typography></li>
+                   <li><Typography>Can Delete: {del}</Typography></li>
+                   <li><Typography>Can Edit Config: {config}</Typography></li>
                 </ul>
-            </div>
+            </Paper>
         )
     }
 
@@ -93,4 +100,8 @@ export default class GeneralSettings extends Component {
             </div>
         )
     }
+}
+
+function yesOrNo( bool ) {
+    return bool ? 'Y' : 'N'
 }
