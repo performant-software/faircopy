1689310cfc6f78f4086cc291e79ef7b304ba0606
Nick Laiacona <nick@performantsoftware.com>
2023-07-11 12:46:21 -0400

    refactor test

--- tests/idmap.test.js
+++ tests/idmap.test.js
@@ -2,39 +2,56 @@ const { createIDMapAuthority } = require('../public/main-process/IDMapAuthority'
 const { getBlankResourceMap } = require('../public/main-process/id-map-authority')
 const { v4: uuidv4 } = require('uuid')
 
-test('test local move resource', () => {
+describe('test local move resource', () => {
     const onUpdate = jest.fn();
     const idMap = createIDMapAuthority(false, "{}", onUpdate ) 
     
     const parentResourceID = uuidv4()
     const nextParentID = uuidv4()
-    idMap.addResource( 'testDoc', null, getBlankResourceMap(parentResourceID, 'teidoc'))
-    idMap.addResource( 'images', 'testDoc', getBlankResourceMap(uuidv4(), 'facs') )
-    idMap.addResource( 'transcription', 'testDoc', getBlankResourceMap(uuidv4(), 'sourceDoc') )
-    idMap.addResource( 'translation', 'testDoc', getBlankResourceMap(uuidv4(), 'text') )
-    idMap.addResource( 'nextParent', null, getBlankResourceMap(nextParentID, 'teidoc'))
-    idMap.sendIDMapUpdate() 
+    const imagesResourceMap = getBlankResourceMap(uuidv4(), 'facs')
 
-    expect(onUpdate).toHaveBeenCalled()
-    expect(Object.keys(idMap.idMap).length).toBe(2)
-    expect(Object.keys(idMap.idMap['testDoc'].ids).length).toBe(3)
-    expect(Object.keys(idMap.idMap['nextParent'].ids).length).toBe(0)
+    test('initial setup', () => {
+        idMap.addResource( 'testDoc', null, getBlankResourceMap(parentResourceID, 'teidoc'))
+        idMap.addResource( 'images', 'testDoc', imagesResourceMap )
+        idMap.addResource( 'transcription', 'testDoc', getBlankResourceMap(uuidv4(), 'sourceDoc') )
+        idMap.addResource( 'translation', 'testDoc', getBlankResourceMap(uuidv4(), 'text') )
+        idMap.addResource( 'nextParent', null, getBlankResourceMap(nextParentID, 'teidoc'))
+        idMap.sendIDMapUpdate() 
+    
+        expect(onUpdate).toHaveBeenCalled()
+        expect(Object.keys(idMap.idMap).length).toBe(2)
+        expect(Object.keys(idMap.idMap['testDoc'].ids).length).toBe(3)
+        expect(Object.keys(idMap.idMap['nextParent'].ids).length).toBe(0)    
+    })
 
-    idMap.moveResourceMap( 'translation', 'translation', nextParentID, parentResourceID )
-    idMap.sendIDMapUpdate() 
+    test('test moving from one teidoc to another', () => {
+        idMap.moveResourceMap( 'translation', 'translation', nextParentID, parentResourceID )
+        idMap.sendIDMapUpdate() 
 
-    expect(onUpdate).toHaveBeenCalled()
-    expect(Object.keys(idMap.idMap).length).toBe(2)
-    expect(Object.keys(idMap.idMap['testDoc'].ids).length).toBe(2)
-    expect(Object.keys(idMap.idMap['nextParent'].ids).length).toBe(1)
+        expect(onUpdate).toHaveBeenCalledTimes(2)
+        expect(Object.keys(idMap.idMap).length).toBe(2)
+        expect(Object.keys(idMap.idMap['testDoc'].ids).length).toBe(2)
+        expect(Object.keys(idMap.idMap['nextParent'].ids).length).toBe(1)    
+    })
 
-    idMap.moveResourceMap( 'nextName', 'transcription', nextParentID, parentResourceID )
-    idMap.sendIDMapUpdate() 
+    test('test moving while changing localID', () => {
+        idMap.moveResourceMap( 'nextName', 'transcription', nextParentID, parentResourceID )
+        idMap.sendIDMapUpdate() 
 
-    expect(onUpdate).toHaveBeenCalled()
-    expect(Object.keys(idMap.idMap).length).toBe(2)
-    expect(Object.keys(idMap.idMap['testDoc'].ids).length).toBe(1)
-    expect(Object.keys(idMap.idMap['nextParent'].ids).length).toBe(2)
-    expect(idMap.idMap['nextParent'].ids['nextName']).not.toBe(null)
+        expect(onUpdate).toHaveBeenCalledTimes(3)
+        expect(Object.keys(idMap.idMap).length).toBe(2)
+        expect(Object.keys(idMap.idMap['testDoc'].ids).length).toBe(1)
+        expect(Object.keys(idMap.idMap['nextParent'].ids).length).toBe(2)
+        expect(idMap.idMap['nextParent'].ids['nextName']).not.toBe(null)        
+    })
 
+    test('test moving an unsaved resourceMap', () => {
+        idMap.setResourceMap( imagesResourceMap, 'images', 'testDoc' )
+        idMap.moveResourceMap( 'images', 'images', nextParentID, parentResourceID )
+
+        expect(onUpdate).toHaveBeenCalledTimes(4)
+        expect(Object.keys(idMap.idMapNext['testDoc'].ids).length).toBe(0)
+        expect(Object.keys(idMap.idMapBase['testDoc'].ids).length).toBe(0)
+        expect(Object.keys(idMap.idMapBase['nextParent'].ids).length).toBe(3)    
+    })
 })
