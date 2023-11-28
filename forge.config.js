67f1597481f34265a67d37fa4beeac70b8be8fd9
Nick Laiacona <nick@performantsoftware.com>
2023-11-28 12:56:44 -0500

    make dmg wip

--- forge.config.js
+++ forge.config.js
@@ -1,35 +1,24 @@
 module.exports = {
   packagerConfig: {
     asar: true,
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    }
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        certificateFile: './cert.pfx',
-        certificatePassword: process.env.CERTIFICATE_PASSWORD
-      }
-    },
-    {
       name: '@electron-forge/maker-dmg',
       config: {
         background: './assets/dmg-background.png',
+        icon: 'build/icon.png',
         format: 'ULFO'
       }
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    }
   ],
   plugins: [
     {
