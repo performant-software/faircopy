ee7c0097b26a9b74138829fa9d22d881536ef0f3
Nick Laiacona <nick@performantsoftware.com>
2024-05-14 08:48:36 -0400

    adjust style

--- src/model/editioncrafter/svg.js
+++ src/model/editioncrafter/svg.js
@@ -1,4 +1,4 @@
-const style = "'stroke:#fff;stroke-dasharray:5;stroke-opacity:0.3;stroke-width:1;fill:transparent;'"
+const style = "'stroke:#fff;stroke-dasharray:2;stroke-opacity:1;stroke-width:1.5;fill:transparent;'"
 
 export function buildSquareFragment(ulx, uly, lrx, lry) {
   return `xywh=pixel:${ulx},${uly},${lrx - ulx},${lry - uly}`
