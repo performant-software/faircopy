f927eb975c713fac42d4f7c1c10c20223afb69c5
Nick Laiacona <nick@performantsoftware.com>
2024-06-28 08:15:03 -0400

    bug fix

--- forge.config.example.js
+++ forge.config.example.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
     packagerConfig: {
       asar: true,
-      icon: './render/icons/icon',
+      icon: './src/render/icons/icon',
       osxSign: {
         optionsForFile: (filePath) => {
           return {
