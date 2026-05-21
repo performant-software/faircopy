99b635009c6e6b8514804e0ce3817856e37420f5
Rebecca Black <rebecca.ann.black@gmail.com>
2026-05-21 10:30:17 -0400

    tooltip text tweaks for automated workflow button

--- src/render/components/main-window/tei-editor/EditorToolbar.jsx
+++ src/render/components/main-window/tei-editor/EditorToolbar.jsx
@@ -168,9 +168,9 @@ export default class EditorToolbar extends Component {
                     { this.renderButton("Find", "fas fa-magnifying-glass", this.onFind ) }
                 </div>
                 <div className="rightgroup">
+                    { this.renderButton("Run Automated Workflow", "fas fa-robot", this.onAgent, canRunAgent ) }
                     { this.renderButton("Edit Properties", "fas fa-edit", onEditResource ) }
                     { this.renderButton("Save", "fas fa-save", onSave, changedSinceLastSave ) }
-                    { this.renderButton("Perform Agent on Document", "fas fa-robot", this.onAgent, canRunAgent ) }
                 </div>
                 { elementMenuOptions && <ElementMenu
                         menus={menus}
